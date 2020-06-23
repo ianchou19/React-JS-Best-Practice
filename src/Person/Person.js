deb465635fb9f0e32ece3ff65c5224b24fc82ab5
Ian Chou <ianchou19@gmail.com>
2020-06-23 17:30:49 -0400

    Understanding the "children" Prob

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const person = (props) => {
   return (
-    <p>
-      I'm {props.name} Person and I am {props.age} years old
-    </p>
+    <div>
+      <p>
+        I'm {props.name} Person and I am {props.age} years old
+      </p>
+      <p>{props.children}</p>
+    </div>
   );
 };
 
