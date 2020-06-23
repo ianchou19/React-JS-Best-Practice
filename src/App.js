9b7203936f23f51aedf5d3334ce765453b4d9b53
Ian Chou <ianchou19@gmail.com>
2020-06-23 17:00:14 -0400

    JSX Restriction

--- src/App.js
+++ src/App.js
@@ -3,15 +3,11 @@ import "./App.css";
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1>Hi, I'm a React App</h1>
-    //   </div>
-    // );
-    return React.createElement(
-      "div",
-      { className: "App" },
-      React.createElement("h1", null, "Hi, I'm a React App")
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p> This is really working</p>
+      </div>
     );
   }
 }
