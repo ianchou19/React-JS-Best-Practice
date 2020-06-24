43c6a45ea360028250d94d779d9201c3b99cc9af
Ian Chou <ianchou19@gmail.com>
2020-06-24 12:42:11 -0400

    Using shouldComponentUpdate for Optimization

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -3,35 +3,7 @@ import React, { Component } from "react";
 import classes from "./Person.css";
 
 class Person extends Component {
-  static getDerivedStateFromProps(props, state) {
-    console.log("[Person.js] getDerivedStateFromProps");
-  }
-
-  componentWillReceiveProps(props) {
-    console.log("[Person.js] componentWillReceiveProps", props);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log("[Person.js] shouldComponentUpdate");
-    return true;
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState) {
-    console.log("[Person.js] getSnapshotBeforeUpdate");
-    return { message: "snapshot!" };
-  }
-
-  // componentWillUpdate() {
-
-  // }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    console.log("[Person.js] componentDidUpdate");
-    console.log(snapshot);
-  }
-
   render() {
-    console.log("[Person.js] rendering...");
     return (
       <div className={classes.Person}>
         <p onClick={this.props.click}>
