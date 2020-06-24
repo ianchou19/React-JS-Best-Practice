ff8add03137648afa5408c2717a1783f9c08a1ac
Ian Chou <ianchou19@gmail.com>
2020-06-24 15:17:40 -0400

    Another Form of HOCs

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,7 +1,11 @@
 import React from "react";
 
-const withClass = (props) => (
-  <div className={props.classes}>{props.children}</div>
-);
+const withClass = (WrapperdComponent, className) => {
+  return (props) => (
+    <div className={className}>
+      <WrapperdComponent />
+    </div>
+  );
+};
 
 export default withClass;
