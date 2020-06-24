9156479bd8305b8f8928d003e919f44a50b2c492
Ian Chou <ianchou19@gmail.com>
2020-06-24 16:30:15 -0400

    Understanding Prop Chain Problems

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,20 +6,35 @@ import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     return (
-      <Fragment>
+      <Aux>
+        {this.props.isAuth ? <p>Authenticated!</p> : <p>Please log in</p>}
         <p key="i1" onClick={this.props.click}>
           I'm {this.props.name} Person and I am {this.props.age} years old
         </p>
         <p key="i2">{this.props.children}</p>
         <input
           key="i3"
+          // ref={(inputEl) => {
+          //   this.inputElement = inputEl;
+          // }}
+          ref={this.inputElementRef}
           type="text"
           onChange={this.props.changed}
           value={this.props.name}
         />
-      </Fragment>
+      </Aux>
     );
   }
 }
--- src/containers/App.js
+++ src/containers/App.js
@@ -20,6 +20,7 @@ class App extends Component {
     showPersons: false,
     showCockpit: true,
     changeCounter: 0,
+    authenticated: false,
   };
 
   static getDerivedStateFromProps(props, state) {
@@ -78,6 +79,10 @@ class App extends Component {
     this.setState({ showPersons: !doesShow });
   };
 
+  loginHandler = () => {
+    this.setState({ authenticated: true });
+  };
+
   render() {
     console.log("[App.js] render");
     let persons = null;
@@ -88,6 +93,7 @@ class App extends Component {
           persons={this.state.persons}
           clicked={this.deletePersonHandler}
           changed={this.nameChangedHandler}
+          isAuthenticated={this.state.authenticated}
         />
       );
     }
@@ -107,6 +113,7 @@ class App extends Component {
             showPersons={this.state.showPersons}
             persons={this.state.persons}
             clicked={this.togglePersonsHandler}
+            login={this.loginHandler}
           />
         ) : null}
         {persons}
