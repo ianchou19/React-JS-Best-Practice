39d4fd18f14dfacee14e05d3d6d92131035fc394
Ian Chou <ianchou19@gmail.com>
2020-06-24 15:24:36 -0400

    Passing Unknown Props

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 
 import Aux from "../../../hoc/Aux";
+import withClass from "../../../hoc/withClass";
 import classes from "./Person.css";
 
 class Person extends Component {
@@ -22,4 +23,4 @@ class Person extends Component {
   }
 }
 
-export default Person;
+export default withClass(Person, classes.Person);
--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -3,7 +3,7 @@ import React from "react";
 const withClass = (WrapperdComponent, className) => {
   return (props) => (
     <div className={className}>
-      <WrapperdComponent />
+      <WrapperdComponent {...props} />
     </div>
   );
 };
