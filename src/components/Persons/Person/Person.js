64b21edcf417d842c46d99720edcecabaccb0db5
Ian Chou <ianchou19@gmail.com>
2020-06-24 11:46:30 -0400

    Component Update Lifecycle (for props Changes)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,18 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 
 import classes from "./Person.css";
 
-const person = (props) => {
-  console.log("[Person.js] rendering...");
-  return (
-    <div className={classes.Person}>
-      <p onClick={props.click}>
-        I'm {props.name} Person and I am {props.age} years old
-      </p>
-      <p>{props.children}</p>
-      <input type="text" onChange={props.changed} value={props.name} />
-    </div>
-  );
-};
+class Person extends Component {
+  static getDerivedStateFromProps(props, state) {
+    console.log("[Person.js] getDerivedStateFromProps");
+  }
 
-export default person;
+  componentWillReceiveProps(props) {
+    console.log("[Person.js] componentWillReceiveProps", props);
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Person.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Person.js] getSnapshotBeforeUpdate");
+    return { message: "snapshot!" };
+  }
+
+  // componentWillUpdate() {
+
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Person.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Person.js] rendering...");
+    return (
+      <div className={classes.Person}>
+        <p onClick={this.props.click}>
+          I'm {this.props.name} Person and I am {this.props.age} years old
+        </p>
+        <p>{this.props.children}</p>
+        <input
+          type="text"
+          onChange={this.props.changed}
+          value={this.props.name}
+        />
+      </div>
+    );
+  }
+}
+
+export default Person;
